8af0c0771d57344fb0e832f166bbed277fb7cf4d
jhs512 <jangka512@gmail.com>
2024-03-08 12:07:13 +0900

    work

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,92 +1,38 @@
 package com.ll;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     private Scanner scanner;
-    private ArrayList<Todo> todos;
-    private long todosLastId;
+    private TodoController todoController;
+    private SystemController systemController;
 
     public App() {
         scanner = new Scanner(System.in);
-        todos = new ArrayList<>();
-        todosLastId = 0;
-    }
+        systemController = new SystemController();
+        todoController = new TodoController();
 
-    public void add() {
-        long id = todosLastId + 1;
-        System.out.print("할일 : ");
-        String content = scanner.nextLine().trim();
-
-        Todo todo = new Todo(id, content);
-        todos.add(todo);
-        todosLastId++;
-
-        System.out.printf("%d번 할일이 생성되었습니다.\n", id);
-    }
-
-    public void list() {
-        System.out.println("번호 / 내용");
-
-        todos.forEach(todo -> System.out.printf("%d / %s\n", todo.getId(), todo.getContent()));
-    }
-
-    public void del() {
-        System.out.print("삭제할 할일의 번호 : ");
-        long id = Long.parseLong(scanner.nextLine().trim());
-
-        boolean isRemoved = todos.removeIf(todo -> todo.getId() == id);
-
-        if (!isRemoved) {
-            System.out.printf("%d번 할일은 존재하지 않습니다.\n", id);
-            return;
-        }
-
-        System.out.printf("%d번 할일이 삭제되었습니다.\n", id);
-    }
-
-    public void modify() {
-        System.out.print("수정할 할일의 번호 : ");
-        long id = Long.parseLong(scanner.nextLine().trim());
-
-        Todo foundTodo = todos.stream()
-                .filter(t -> t.getId() == id)
-                .findFirst()
-                .orElse(null);
-
-        if (foundTodo == null) {
-            System.out.printf("%d번 할일은 존재하지 않습니다.\n", id);
-            return;
-        }
-
-        System.out.printf("기존 할일 : %s\n", foundTodo.getContent());
-        System.out.print("새 할일 : ");
-        foundTodo.setContent(scanner.nextLine().trim());
-
-        System.out.printf("%d번 할일이 수정되었습니다.\n", id);
     }
 
     public void run() {
-        System.out.println("할일 관리 앱, 시작");
-
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine().trim();
 
-            if (cmd.equals("exit")) break;
-            else if (cmd.equals("add")) {
-                add();
+            if (cmd.equals("exit")) {
+                systemController.exit();
+                break;
+            } else if (cmd.equals("add")) {
+                todoController.add();
             } else if (cmd.equals("list")) {
-                list();
+                todoController.list();
             } else if (cmd.equals("del")) {
-                del();
+                todoController.del();
             } else if (cmd.equals("modify")) {
-                modify();
+                todoController.modify();
             }
         }
 
-        System.out.println("할일 관리 앱, 끝");
         scanner.close();
     }
 }
